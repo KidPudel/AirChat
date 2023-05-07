a968ec1dc745d328db2ffa67b12f95bd713ff408
Iggy Kuperson <I.Kupchinenko@gmail.com>
2023-05-07 12:08:36 +0300

    making it's an android app

--- shared/src/commonMain/kotlin/com/iggydev/airchat/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.iggydev.airchat
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/iggydev/airchat/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.iggydev.airchat
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
